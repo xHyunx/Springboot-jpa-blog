c241157082c36780cc3a954366c3d457e4d24ac1
Hyun <ncj223@naver.com>
2023-07-18 15:50:11 +0900

    ResponseDto 추가

---
+++ src/main/java/com/cos/blog/dto/ResponseDto.java
@@ -0,0 +1,17 @@
+package com.cos.blog.dto;
+
+import org.springframework.http.HttpStatus;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class ResponseDto<T> {
+	int status;
+	T data;
+}
