a53a0d69cac24295007181c2268cb03d8e55a809
Hyun <hyun998832@gmail.com>
2023-07-23 17:48:59 +0900

    로그인 ajax (스프링 시큐리티 X)

--- src/main/resources/static/js/user.js
+++ src/main/resources/static/js/user.js
@@ -3,6 +3,10 @@ let index = {
 		$("#btn-save").on("click", ()=>{ // 화살표 함수 사용이유는 this를 바인딩하기 위해서임
 			this.save();
 		});
+		
+		$("#btn-login").on("click", () => {
+			this.login();
+		});
 	},	
 	
 	save : function() {
@@ -17,13 +21,34 @@ let index = {
 		// ajax 통신을 이용해서 3개의 데이터를 json으로 변경하여 insert 요청
 		$.ajax({
 			type: "POST",
-			url:"/blog/api/user",
+			url:"/api/user",
 			data: JSON.stringify(data),
 			contentType : "application/json; charset=utf-8",
 			dataType : "json"
 		}).done(function(resp){ // 요청을 서버로부터 응답이 왔을 때 기본적으로 모든 것이 문자열(생긴게 json이라면) => javascript 오브젝트
 			alert("회원가입이 완료되었습니다.");
-			location.href = "/blog";
+			location.href = "/";
+		}).fail(function(error){
+			alert(JSON.stringify(error));
+		});
+	},
+	
+	login : function() {
+
+		let data = {
+			username : $("#username").val(),
+			password : $("#password").val(),
+		}
+		
+		$.ajax({
+			type: "POST",
+			url:"/api/user/login",
+			data: JSON.stringify(data),
+			contentType : "application/json; charset=utf-8",
+			dataType : "json"
+		}).done(function(resp){ // 요청을 서버로부터 응답이 왔을 때 기본적으로 모든 것이 문자열(생긴게 json이라면) => javascript 오브젝트
+			alert("로그인이 완료되었습니다.");
+			location.href = "/";
 		}).fail(function(error){
 			alert(JSON.stringify(error));
 		});
